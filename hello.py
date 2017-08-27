f033e871be3e21b30a1cb3bb0cde3306049be747
congor <31139258+congor@users.noreply.github.com>
2017-08-27 21:37:21 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,4 +2,4 @@ def hello_app(environ, start_response):
     status = '200 OK'
     headers = [('Content-type', 'text/plain')]
     start_response (status, headers)
-    return ["\n".join(environ.get('QUERY_STRING'.split("&"))]
+    return ["\n".join(environ.get('QUERY_STRING').split("&"))]
