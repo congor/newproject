c6645738b3cbc5b9c63fec9c7d074fc208f6038f
Your Name <gorohovkm@gmail.com>
2017-08-27 19:45:24 +0500

    one

---
+++ hello.py
@@ -0,0 +1,5 @@
+def hello_app(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain')]
+    start_response (status, headers)
+    return ["\n".join(environ.get('QUERY_STRING'.split("&"))]
